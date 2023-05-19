a25f0a122ab803a1177cbf3c0e95997c49780991
anagon <ana.cristina.gonzalez.sanchez@ki.se>
2023-05-19 13:46:27 +0200

    Added an extra test to save output in a user-specified path rather than automatically generated. Tries the save_activepixels option.

--- test/test_detection.py
+++ test/test_detection.py
@@ -3,14 +3,14 @@ import astroCAST
 from pathlib import Path
 import os
 
-def test_run_function(threshold = None, save_actpixels = False):
-    det = Detector("/Volumes/tank/groups/herlenius/anagon/22A7x7-1.h5")
-    det.run(dataset = "dff/ast", use_dask = True)
+def test_run_function(threshold = None, save_activepixels = False, output = None):
+    det = Detector("/Volumes/tank/groups/herlenius/anagon/22A7x7-1.h5", output = output)
+    det.run(dataset = "dff/ast", use_dask = True, save_activepixels = save_activepixels)
 
     # Assert function's expected behaviour
     assert os.path.exists(f"{det.output_directory}/event_map.tdb"), "event map file does not exist in output_directory"
     assert os.path.exists(f"{det.output_directory}/event_map.tiff"), "tiff output file does not exist in output_directory"
-    if save_actpixels == True: 
+    if save_activepixels == True: 
         assert os.path.exists(f"{det.output_directory}/active_pixels.tiff"), "Active pixels file does not exist in output directory"
     assert os.path.exists(f"{det.output_directory}/time_map.npy"), "time_map.npy file does not exist in output directory"
     assert os.path.exists(f"{det.output_directory}/events.npy"), "Combined events file does not exist in the output directory"
@@ -21,3 +21,4 @@ def test_run_function(threshold = None, save_actpixels = False):
     assert det.data.shape is not None, "data has no dimensions"
 test_run_function()
 
+test_run_function(save_activepixels = True, output = "/Users/anagon/Desktop/output.ast.roi")
