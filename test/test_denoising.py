55b2e6f45efb7ef5ff38e45c06f49f2eff2cbf24
Jan Philipp Reising <jan.reising@ki.se>
2023-08-23 12:14:32 +0200

    fix: tests for denoising.py

--- test/test_denoising.py
+++ test/test_denoising.py
@@ -1,8 +1,15 @@
+import logging
+import tempfile
+from pathlib import Path
+
 import pytest
 
 from astroCAST.denoising import FullFrameGenerator, SubFrameGenerator
 from astroCAST.denoising import Network
 import os
+
+from astroCAST.preparation import IO
+
 
 @pytest.mark.parametrize("pre_post_frame", [5, (3, 2)])
 def test_generator_full_frame(file_path, pre_post_frame):
@@ -56,28 +63,49 @@ def test_inference_full(file_path, out_path=None):
         assert os.path.isfile(out_path), "cannot find output tiff: {}".format(out_path)
         os.remove(out_path)
 
-def test_inference_sub(file_path, out_path=None):
+@pytest.mark.parametrize("output_file", [None, "inf.tiff", "inf.h5"])
+def test_inference_sub(file_path, output_file, loc="data/ch0"):
 
-    param = dict(paths=file_path, loc="data/ch0", input_size=(25, 25), pre_post_frame=5, gap_frames=0,
-                 normalize="global", cache_results=True, in_memory=True)
+    with tempfile.TemporaryDirectory() as tmpdir:
 
-    train_gen = SubFrameGenerator(padding=None, batch_size=25, max_per_file=50,
-                                   allowed_rotation=[1, 2, 3], allowed_flip=[0, 1], shuffle=True, **param)
-    val_gen = SubFrameGenerator(padding=None, batch_size=25, max_per_file=5,
-                                   allowed_rotation=[0], allowed_flip=[-1], shuffle=True, **param)
+        tmpdir = Path(tmpdir)
+        assert tmpdir.is_dir()
 
-    net = Network(train_generator=train_gen, val_generator=val_gen, n_stacks=1, kernel=8, batchNormalize=False, use_cpu=True)
-    net.run(batch_size=train_gen.batch_size, num_epochs=2, patience=1, min_delta=0.01, save_model=None, load_weights=False)
-    model = net.model
+        if output_file is None:
+            out_path = None
+        else:
+            out_path = tmpdir.joinpath(output_file)
 
-    inf_gen = SubFrameGenerator(padding="edge", batch_size=25, allowed_rotation=[0], allowed_flip=[-1],
-                                shuffle=False, max_per_file=2, **param)
+        param = dict(paths=file_path, loc=loc, input_size=(25, 25), pre_post_frame=5, gap_frames=0,
+                     normalize="global", cache_results=True, in_memory=True)
 
-    # out_path = "testdata/sample_0_inf.tiff"
-    inf_gen.infer(model=model, output=out_path, rescale=False)
+        train_gen = SubFrameGenerator(padding=None, batch_size=25, max_per_file=50,
+                                       allowed_rotation=[1, 2, 3], allowed_flip=[0, 1], shuffle=True, **param)
+        val_gen = SubFrameGenerator(padding=None, batch_size=25, max_per_file=5,
+                                       allowed_rotation=[0], allowed_flip=[-1], shuffle=True, **param)
 
-    if out_path is not None:
-        assert os.path.isfile(out_path), "cannot find output tiff: {}".format(out_path)
-        os.remove(out_path)
+        net = Network(train_generator=train_gen, val_generator=val_gen, n_stacks=1, kernel=8, batchNormalize=False, use_cpu=True)
+        net.run(batch_size=train_gen.batch_size, num_epochs=2, patience=1, min_delta=0.01, save_model=None, load_weights=False)
+        model = net.model
+
+        inf_gen = SubFrameGenerator(padding="edge", batch_size=25,
+                                    allowed_rotation=[0], allowed_flip=[-1],
+                                    shuffle=False,
+                                    logging_level=logging.DEBUG,
+                                    **param)
+
+        res = inf_gen.infer(model=model, output=out_path, out_loc=loc, rescale=False)
+
+        # Check result
+        io = IO()
+        data = io.load(file_path, h5_loc=loc, lazy=True)
+
+        if out_path is not None:
+            assert out_path.is_file(), "cannot find output file: {}".format(out_path)
+            res = io.load(out_path, h5_loc=loc, lazy=True)
+
+        assert res.shape == data.shape, f"inferred output has incorrect shape: " \
+                                            f"orig>{data.shape} vs. inf>{res.shape}"
+
 
 
