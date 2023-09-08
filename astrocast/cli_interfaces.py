4d7d3a93b31478ce06b15d22292eb32369ee3eb0
Jan Philipp Reising <jan.reising@ki.se>
2023-09-08 11:30:15 +0200

    - add: inference for output files when given extension instead of path - fix: CLI doesn't throw error but logs error instead. Fixes error for chained pipeline commands - add: flexible parsing of chunks parameter - reformat: moved convert-input interface further up in the code. adhering to expected sequence of the pipeline

--- astrocast/cli_interfaces.py
+++ astrocast/cli_interfaces.py
@@ -1,6 +1,7 @@
 import datetime as dt
 import logging
 import os
+import shutil
 import time
 from pathlib import Path
 
@@ -27,23 +28,37 @@ def check_output(output_path, input_path, h5_loc_save, overwrite):
 
     output_path = Path(output_path)
     input_path = Path(input_path)
+
+    if output_path.name.startswith("."):
+        output_path = input_path.with_suffix(output_path.name)
+        logging.warning(f"Output path inferred as: {output_path}")
+
     if output_path.exists():
 
         if output_path.suffix in (".hdf5", ".h5"):
             with h5py.File(output_path.as_posix(), "r") as f:
                 if h5_loc_save in f and not overwrite:
-                    raise FileExistsError(f"{h5_loc_save} already exists in {output_path}. "
+                    logging.error(f"{h5_loc_save} already exists in {output_path}. "
                                           f"Please choose a different output location or use '--overwrite True'")
+                    return 0
 
         else:
-            raise FileExistsError(f"file already exists {output_path}. Please choose a different output location "
+            logging.error(f"file already exists {output_path}. Please choose a different output location "
                                   f"or use '--overwrite True'.")
-
-    if output_path.name.startswith("."):
-        output_path = input_path.with_suffix(output_path)
-        logging.warning(f"Output path inferred as: {output_path}")
+            return 0
 
     return output_path
+
+def parse_chunks(chunks):
+
+    if chunks is None or chunks == "infer":
+        return chunks
+
+    else:
+        chunks = tuple(int(c) for c in chunks.split(","))
+        if len(chunks) != 3:
+            raise ValueError(f"please provide 'chunks' parameter as None, infer or comma-separated list of 3 int values.")
+        return chunks
 
 @click.group(context_settings={'auto_envvar_prefix': 'CLI'}, chain=True)
 @click.option('--config', default=None, type=click.Path())  # this allows us to change config path
@@ -62,6 +77,52 @@ def cli(ctx, config):
 
 @cli.command()
 @click.argument('input-path', type=click.Path(exists=True))
+@click_custom_option('--logging-level', type=click.INT, default=logging.INFO, help='Logging level for messages.')
+@click_custom_option('--output-path', type=click.Path(), help='Path to save the processed data. If None, the processed data is returned.')
+@click_custom_option('--sep', default="_", help='Separator used for sorting file names.')
+@click_custom_option('--channels', default=1, help='Number of channels or dictionary specifying channel names.')
+@click_custom_option('--z-slice', default=None, help='Z slice index.')
+@click_custom_option('--lazy', is_flag=True, help='Lazy loading flag.')
+@click_custom_option('--subtract-background', default=None, help='Background subtraction parameter.')
+@click_custom_option('--subtract-func', default="mean", help='Function to use for background subtraction.')
+@click_custom_option('--rescale', default=None, help='Rescale parameter.')
+@click_custom_option('--dtype', default=np.uint, help='Data type to convert the processed data.')
+@click_custom_option('--in-memory', is_flag=True, help='If True, the processed data is loaded into memory.')
+@click_custom_option('--h5-loc', default="data", help='Prefix to use when saving the processed data.')
+@click_custom_option('--chunks', type=click.STRING, default="infer", help='Chunk size to use when saving to HDF5 or TileDB.')
+@click_custom_option('--compression', default=None, help='Compression method to use when saving to HDF5 or TileDB.')
+@click_custom_option('--overwrite', type=click.BOOL, default=False, help='Flag for overwriting previous result in output location')
+def convert_input(input_path, logging_level, output_path, sep, channels, z_slice, lazy, subtract_background,
+                  subtract_func, rescale, dtype, in_memory, h5_loc, chunks, compression, overwrite):
+
+    """
+    Convert user files to astroCAST compatible format using the Input class.
+    """
+
+    logging.basicConfig(level=logging_level)
+    t0 = time.time()
+
+    # check output
+    output_path = check_output(output_path, input_path, h5_loc, overwrite)
+    if output_path == 0:
+        logging.warning("skipping this step because output exists.")
+        return 0
+
+    # check chunks
+    chunks = parse_chunks(chunks)
+
+    # convert input
+    input_instance = Input(logging_level=logging_level)
+    input_instance.run(input_path=input_path, output_path=output_path, sep=sep, channels=channels, z_slice=z_slice,
+                       lazy=lazy, subtract_background=subtract_background, subtract_func=subtract_func, rescale=rescale,
+                       dtype=dtype, in_memory=in_memory, h5_loc=h5_loc, chunks=chunks, compression=compression)
+
+    # logging
+    delta = humanize.naturaldelta(dt.timedelta(seconds=time.time() - t0))
+    logging.info(f"Motion correction finished in {delta}")
+
+@cli.command()
+@click.argument('input-path', type=click.Path())
 @click_custom_option('--output-path', type=None, help='Path to save the output data.')
 @click_custom_option('--working-directory', type=click.Path(), default=None, help='Working directory for temporary files.')
 @click_custom_option('--logging-level', type=click.INT, default=logging.INFO, help='Logging level for messages.')
@@ -80,7 +141,7 @@ def cli(ctx, config):
 @click_custom_option('--nonneg-movie', type=click.BOOL, default=True, help='Make the saved movie and template mostly nonnegative by removing min_mov from movie.')
 @click_custom_option('--gsig-filt', type=click.Tuple([int, int]), default=(20, 20), help='Tuple indicating the size of the filter.')
 @click_custom_option('--h5-loc-save', type=click.STRING, default="mc", help='Location within the HDF5 file to save the data.')
-@click_custom_option('--chunks', type=click.Tuple([int, int, int]), default=None, help='Chunk shape for creating a dask array when saving to an HDF5 file.')
+@click_custom_option('--chunks', type=click.STRING, default=None, help='Chunk shape for creating a dask array when saving to an HDF5 file.')
 @click_custom_option('--compression', type=click.STRING, default=None, help='Compression algorithm to use when saving to an HDF5 file.')
 @click_custom_option('--overwrite', type=click.BOOL, default=False, help='Flag for overwriting previous result in output location')
 def motion_correction(input_path, working_directory, logging_level, output_path, h5_loc,
@@ -96,6 +157,12 @@ def motion_correction(input_path, working_directory, logging_level, output_path,
 
     # check output
     output_path = check_output(output_path, input_path, h5_loc_save, overwrite)
+    if output_path == 0:
+        logging.warning("skipping this step because output exists.")
+        return 0
+
+    # check chunks
+    chunks = parse_chunks(chunks)
 
     # Initialize the MotionCorrection instance
     logging.info("creating motion correction instance ...")
@@ -116,15 +183,13 @@ def motion_correction(input_path, working_directory, logging_level, output_path,
     delta = humanize.naturaldelta(dt.timedelta(seconds=time.time() - t0))
     logging.info(f"Motion correction finished in {delta}")
 
-
 @cli.command()
-@click.argument('input-path', type=click.Path(exists=True))
+@click.argument('input-path', type=click.Path())
 @click_custom_option('--window', type=click.INT, required=True, help='Size of the window for the minimum filter.')
 @click_custom_option('--output-path', type=None, help='Path to save the output data.')
 @click_custom_option('--loc', type=click.STRING, default="", help='Location of the data in the HDF5 file (if applicable).')
 @click_custom_option('--method', type=click.Choice(['background', 'dF', 'dFF']), default='background', help='Method to use for delta calculation.')
-# @click_custom_option('--chunks', type=click.Tuple([int, str, str]), default=(1, 100, 100), help='Chunk size for data processing.')
-@click_custom_option('--chunks', default="infer", help='Chunk size for data processing.')
+@click_custom_option('--chunks', type=click.STRING, default="infer", help='Chunk size for data processing.')
 @click_custom_option('--overwrite-first-frame', type=click.BOOL, default=True, help='Whether to overwrite the first frame with the second frame after delta calculation.')
 @click_custom_option('--lazy', type=click.BOOL, default=True, help='Flag for lazy data loading and computation.')
 @click_custom_option('--h5-loc', type=click.STRING, default="df", help='Location within the HDF5 file to save the data.')
@@ -142,6 +207,12 @@ def subtract_delta(input_path, output_path, loc, method, window, chunks, overwri
 
     # check output
     output_path = check_output(output_path, input_path, h5_loc, overwrite)
+    if output_path == 0:
+        logging.warning("skipping this step because output exists.")
+        return 0
+
+    # check chunks
+    chunks = parse_chunks(chunks)
 
     # Initialize the Delta instance
     logging.info("creating delta instance ...")
@@ -159,50 +230,9 @@ def subtract_delta(input_path, output_path, loc, method, window, chunks, overwri
     delta = humanize.naturaldelta(dt.timedelta(seconds=time.time() - t0))
     logging.info(f"Motion correction finished in {delta}")
 
-
 @cli.command()
-@click.argument('input-path', type=click.Path(exists=True))
-@click_custom_option('--logging-level', type=click.INT, default=logging.INFO, help='Logging level for messages.')
-@click_custom_option('--output-path', type=click.Path(), help='Path to save the processed data. If None, the processed data is returned.')
-@click_custom_option('--sep', default="_", help='Separator used for sorting file names.')
-@click_custom_option('--channels', default=1, help='Number of channels or dictionary specifying channel names.')
-@click_custom_option('--z-slice', default=None, help='Z slice index.')
-@click_custom_option('--lazy', is_flag=True, help='Lazy loading flag.')
-@click_custom_option('--subtract-background', default=None, help='Background subtraction parameter.')
-@click_custom_option('--subtract-func', default="mean", help='Function to use for background subtraction.')
-@click_custom_option('--rescale', default=None, help='Rescale parameter.')
-@click_custom_option('--dtype', default=np.uint, help='Data type to convert the processed data.')
-@click_custom_option('--in-memory', is_flag=True, help='If True, the processed data is loaded into memory.')
-@click_custom_option('--h5-loc', default="data", help='Prefix to use when saving the processed data.')
-@click_custom_option('--chunks', default=None, help='Chunk size to use when saving to HDF5 or TileDB.')
-@click_custom_option('--compression', default=None, help='Compression method to use when saving to HDF5 or TileDB.')
-@click_custom_option('--overwrite', type=click.BOOL, default=False, help='Flag for overwriting previous result in output location')
-def convert_input(input_path, logging_level, output_path, sep, channels, z_slice, lazy, subtract_background,
-                  subtract_func, rescale, dtype, in_memory, h5_loc, chunks, compression, overwrite):
-
-    """
-    Convert user files to astroCAST compatible format using the Input class.
-    """
-
-    logging.basicConfig(level=logging_level)
-    t0 = time.time()
-
-    # check output
-    output_path = check_output(output_path, input_path, h5_loc, overwrite)
-
-    # convert input
-    input_instance = Input(logging_level=logging_level)
-    input_instance.run(input_path=input_path, output_path=output_path, sep=sep, channels=channels, z_slice=z_slice,
-                       lazy=lazy, subtract_background=subtract_background, subtract_func=subtract_func, rescale=rescale,
-                       dtype=dtype, in_memory=in_memory, h5_loc=h5_loc, chunks=chunks, compression=compression)
-
-    # logging
-    delta = humanize.naturaldelta(dt.timedelta(seconds=time.time() - t0))
-    logging.info(f"Motion correction finished in {delta}")
-
-@cli.command()
-@click.argument('input-path', type=click.Path(exists=True))
-@click_custom_option('--model', type=click.Path(exists=True), required=True, help='Path to the trained model file or the model object itself.')
+@click.argument('input-path', type=click.Path())
+@click_custom_option('--model', type=click.Path(), required=True, help='Path to the trained model file or the model object itself.')
 @click_custom_option('--output-file', type=click.Path(), required=True, help='Path to the output file where the results will be saved. If not provided, the result will be returned instead of being saved to a file.')
 @click_custom_option('--batch-size', type=click.INT, default=16, help='batch size processed in each step.')
 @click_custom_option('--input-size', type=(int, int), default=(100, 100), help='size of the denoising window')
@@ -272,11 +302,11 @@ def denoise(input_file, batch_size, input_size, pre_post_frame, gap_frames, z_se
     logging.info(f"Denoising finished in {delta}")
 
 @cli.command()
-@click.argument('input-path', type=click.Path(exists=True))
+@click.argument('input-path', type=click.Path())
 @click_custom_option('--output-path', type=click.Path(), default=None, help='Path to the output file.')
 @click_custom_option('--indices', type=click.STRING, default=None, help='Indices in a numpy array format.')
 @click_custom_option('--logging-level', type=click.INT, default=logging.INFO, help='Logging level for messages.')
-@click_custom_option('--h5_loc', type=click.STRING, default=None, help='Name or identifier of the dataset in the h5 file.')
+@click_custom_option('--h5-loc', type=click.STRING, default=None, help='Name or identifier of the dataset in the h5 file.')
 @click_custom_option('--threshold', type=click.FLOAT, default=None, help='Threshold value to discriminate background from events.')
 @click_custom_option('--min-size', type=click.INT, default=20, help='Minimum size of an event region.')
 @click_custom_option('--lazy', type=click.BOOL, default=True, help='Whether to implement lazy loading.')
@@ -303,11 +333,12 @@ def detect_events(input_path, output_path, indices, logging_level, h5_loc, thres
 
         if overwrite:
             logging.warning(f"overwrite is {overwrite}, deleting previous result")
-            Path(output_path).unlink()
+            shutil.rmtree(output_path)
         else:
             raise FileExistsError(f"Aborting detection because previous calculation exists ({output_path}."
                                   f"Please provide an alternative output path or set '--overwrite True'")
 
+    logging.warning(f"input: {input_path}")
 
     # Initializing the Detector instance
     detector = Detector(input_path=input_path, output=output_path,
@@ -404,7 +435,7 @@ def view_data(input_path, h5_loc, z_select, lazy):
     napari.run()
 
 @cli.command()
-@click.argument('input-path', type=click.Path(exists=True))
+@click.argument('input-path', type=click.Path())
 @click_custom_option('--output-path', type=click.Path(), required=True, help='Path to the output file.')
 @click_custom_option('--h5-loc-in', type=click.STRING, default="", help='Name or identifier of the dataset in the h5 file.')
 @click_custom_option('--h5-loc-out', type=click.STRING, default="", help='Name or identifier of the dataset in the h5 file.')
@@ -440,6 +471,12 @@ def export_video(input_path, output_path, h5_loc_in, h5_loc_out, z_select, lazy,
     Example:
     export_video('input.h5', 'output.h5', h5_loc_in='dataset1', h5_loc_out='dataset2', z_select=(10, 20), lazy=True, chunk_size=(100, 100), compression='gzip', overwrite=True)
     """
+
+    if Path(output_path).exists():
+        logging.error(f"file already exists {output_path}. Please choose a different output location "
+                                  f"or use '--overwrite True'.")
+
+        return 0
 
     io = IO()
     data = io.load(input_path, h5_loc=h5_loc_in, z_slice=z_select, lazy=lazy)
