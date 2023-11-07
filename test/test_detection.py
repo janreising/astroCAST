49766eaeb2089cc28da28c103aed6c74a4e2a578
Jan Philipp Reising <jan.reising@ki.se>
2023-11-07 10:52:11 +0100

    - fix: small tests failing

--- test/test_detection.py
+++ test/test_detection.py
@@ -50,7 +50,8 @@ class Test_Detector:
             h5_loc = "dff/ch0"
 
             sim = EventSim()
-            video, num_events = sim.simulate(shape=(50, 100, 100), skip_n=5, event_intensity=100, background_noise=1)
+            video, num_events = sim.simulate(shape=(50, 100, 100), skip_n=5, gap_space=5, gap_time=3,
+                                             event_intensity=100, background_noise=1)
             io = IO()
             io.save(path=path, data={h5_loc:video})
 
