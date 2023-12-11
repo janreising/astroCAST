fab723e6d7d3650ecec1e738332379e34d46d031
Jan Philipp Reising <jan.reising@ki.se>
2023-12-11 11:08:35 +0100

    Add PyTorch support to model generation scripts
    The `generate_pretrain_models.yaml` and `run_generate_models.py` scripts have been updated to include an option for using PyTorch in the model generation. This option can be toggled through config parameters `use_pytorch` and `use_cpu`. Also, the code has been restructured to conditionally generate training and validation data based on whether PyTorch is being used or not.

--- notebooks/run_generate_models.py
+++ notebooks/run_generate_models.py
@@ -1,19 +1,18 @@
-# Import necessary modules of astroCAST
 import itertools
-import os
-import pathlib
 import sys
 import traceback
+from pathlib import Path
 
 import yaml
 
-from astrocast.denoising import SubFrameGenerator, Network
-from pathlib import Path
+from astrocast.denoising import SubFrameGenerator, Network, SubFrameDataset, PyTorchNetwork
 
 with open("generate_pretrain_models.yaml", "r") as file:
     config = yaml.safe_load(file)
 
 params = config["param"]
+use_pytorch = params["use_pytorch"]
+use_cpu = params["use_cpu"]
 
 root = Path(config["root_path"])
 model_path = Path(config["model_path"])
@@ -53,9 +52,14 @@ for param_set in combinations:
 
         try:
             input_size = param_set["input_size"]
-            pre_post_frame = param_set["pre_post_frame"]
+            pre_post_frames = param_set["pre_post_frames"]
             gap_frames = param_set["gap_frames"]
             train_rotation = param_set["train_rotation"]
+
+            n_stacks, kernel = param_set["architecture"]
+            epochs = param_set["epochs"]
+            patience = param_set["patience"]
+            min_delta = param_set["min_delta"]
 
             # Trainer
             train_str = v["train"]
@@ -64,13 +68,8 @@ for param_set in combinations:
             else:
                 train_paths = root.joinpath(train_str)
 
-            train_gen = SubFrameGenerator(
-                paths=train_paths, max_per_file=v["max_per_file"], loc=v["loc"], input_size=input_size,
-                pre_post_frame=pre_post_frame, gap_frames=gap_frames, allowed_rotation=train_rotation,
-                padding=None, batch_size=8, normalize="global", in_memory=False,
-                allowed_flip=[0, 1], shuffle=True)
-
             # Validator
+            val_paths = None
             if "val" in v:
                 val_str = v["train"]
                 if "*" in val_str:
@@ -78,28 +77,58 @@ for param_set in combinations:
                 else:
                     val_paths = root.joinpath(val_str)
 
-                val_gen = SubFrameGenerator(
-                    paths=val_paths, max_per_file=3, loc=v["loc"], input_size=input_size,
-                    pre_post_frame=pre_post_frame, gap_frames=gap_frames, allowed_rotation=[0],
-                    padding=None, batch_size=16, normalize="global", in_memory=False,
-                    cache_results=True,
-                    allowed_flip=[-1], shuffle=True)
+            if not use_pytorch:
+
+                train_gen = SubFrameGenerator(paths=train_paths, max_per_file=v["max_per_file"], loc=v["loc"],
+                                              input_size=input_size,
+                                              pre_post_frames=pre_post_frames, gap_frames=gap_frames,
+                                              allowed_rotation=train_rotation,
+                                              padding=None, batch_size=8, normalize="global", in_memory=False,
+                                              allowed_flip=[0, 1], shuffle=True)
+
+                # Validator
+                if val_paths is not None:
+
+                    val_gen = SubFrameGenerator(
+                        paths=val_paths, max_per_file=3, loc=v["loc"], input_size=input_size,
+                        pre_post_frames=pre_post_frames, gap_frames=gap_frames, allowed_rotation=[0],
+                        padding=None, batch_size=16, normalize="global", in_memory=False,
+                        cache_results=True,
+                        allowed_flip=[-1], shuffle=True)
+
+                else:
+                    val_gen = None
+
+                # Network
+
+                net = Network(train_generator=train_gen, val_generator=val_gen, learning_rate=0.001, decay_rate=0.99,
+                              pretrained_weights=None,
+                              n_stacks=n_stacks, kernel=kernel,
+                              batchNormalize=False, use_cpu=use_cpu)
+
+                net.run(batch_size=1, num_epochs=epochs, patience=patience, min_delta=min_delta,
+                        save_model=save_model_path)
 
             else:
-                val_gen = None
 
-            # Network
-            n_stacks, kernel = param_set["architecture"]
-            epochs = param_set["epochs"]
-            patience = param_set["patience"]
-            min_delta = param_set["min_delta"]
-            net = Network(train_generator=train_gen, val_generator=val_gen, learning_rate=0.001, decay_rate=0.99,
-                          pretrained_weights = None,
-                          n_stacks=n_stacks, kernel=kernel,
-                          batchNormalize=False, use_cpu=False)
+                train_dataset = SubFrameDataset(paths=train_paths, input_size=input_size, loc=v["loc"],
+                                                pre_post_frames=pre_post_frames, max_per_file=v["max_per_file"],
+                                                gap_frames=gap_frames, allowed_rotation=train_rotation, padding=None,
+                                                normalize="global", in_memory=False, allowed_flip=[0, 1], shuffle=True)
 
-            net.run(batch_size=1, num_epochs=epochs, patience=patience, min_delta=min_delta,
-            save_model= save_model_path)
+                val_dataset = None
+                if "val" in v:
+                    val_dataset = SubFrameDataset(paths=val_paths, input_size=input_size, loc=v["loc"],
+                                                  pre_post_frames=pre_post_frames, max_per_file=3,
+                                                  gap_frames=gap_frames, allowed_rotation=0, padding=None,
+                                                  normalize="global", in_memory=False, allowed_flip=-1, shuffle=True)
+
+                net = PyTorchNetwork(train_dataset, val_dataset=val_dataset, batch_size=16, shuffle=True, num_workers=4,
+                                     learning_rate=0.001, momentum=0.9, decay_rate=0.1, decay_steps=30,
+                                     n_stacks=n_stacks, kernels=kernel, kernel_size=3, batch_normalize=False,
+                                     use_cpu=use_cpu)
+
+                net.run(num_epochs=epochs, save_model=save_model_path, patience=patience, min_delta=min_delta)
 
         except KeyboardInterrupt:
             sys.exit(2)
