2eb9106288d463df758fb3b98b78117cf522773c
Jan Philipp Reising <jan.reising@ki.se>
2024-02-27 18:16:58 +0100

    Add bibliography and logo files
    This commit introduces a new bibliography file (astrocast.bib), containing various references for the manuscript. Additionally, a logo file (logo1.pdf) was added to the manuscript directory. These files will help to enhance the comprehensibility and visual appearance of the manuscript.

---
+++ manuscript/clean_bib_file.py
@@ -0,0 +1,33 @@
+import re
+from pathlib import Path
+
+
+def fix_language_codes(bbl_file_path: Path, output_file_path: Path = None):
+    """Replace 'en' and 'eng' with 'english' in .bbl files.
+
+    Args:
+      bbl_file_path: Path to the original .bbl file.
+      output_file_path: Path to save the modified .bbl file. If None, overwrite the original.
+
+    Raises:
+      FileNotFoundError: If the original .bbl file does not exist.
+    """
+    
+    with open(bbl_file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    
+    # Replace 'en' and 'eng' with 'english', ensuring we don't match parts of words
+    content = re.sub(r'language = \{en\}', r'language = {english}', content)
+    content = re.sub(r'language = \{eng\}', r'language = {english}', content)
+    
+    if output_file_path is None:
+        output_file_path = bbl_file_path
+    
+    with open(output_file_path, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+
+file_path = Path("astrocast.bib")
+assert file_path.exists(), f"{file_path} does not exist"
+
+fix_language_codes(file_path, file_path)
---
+++ manuscript/create_figures.py
@@ -0,0 +1,44 @@
+import re
+from pathlib import Path
+
+figures_directory = Path("./figures")
+output_filename = "figures.tex"
+
+
+# Function to format captions
+def format_caption(caption: str):
+    # Formats strings like (A) to \textbf{(A)}
+    caption = re.sub(r'\(([A-Z])\)', r'\\textbf{(\1)}', caption)
+    caption = caption.replace("%", "\\%")
+    return caption
+
+
+# Start writing to the output file
+with open(output_filename, 'w') as output_file:
+    
+    filenames = figures_directory.glob("*.png")
+    for filename in filenames:
+        
+        name = filename.name
+        base_name = filename.stem
+        caption_filename = filename.with_suffix(".txt")
+        
+        # load caption if exists
+        if not caption_filename.exists():
+            formatted_caption = "\\missing{figure caption}"
+        else:
+            with open(caption_filename, 'r') as caption_file:
+                caption = caption_file.read()
+                formatted_caption = format_caption(caption)
+        
+        # Write the figure inclusion code to the output file
+        output_file.write(
+                f"\\begin{{figure}}[h!]\n"
+                f"\\begin{{center}}\n"
+                f"\\includegraphics[width=\\linewidth]{{figures/{name}}}\n"  # Adjust path if necessary
+                f"\\end{{center}}\n"
+                f"\\caption{{{formatted_caption}}}\label{{fig:{base_name}}}\n"
+                f"\\end{{figure}}\n\n"
+                )
+
+print(f"Figures and captions have been written to {output_filename}.")
